ab8a2b4fdcbd055f1872bbb719baa5606c90e809
JozsefVass <joco.udv@gmail.com>
2021-04-01 07:33:23 +0100

    Initial commit

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <header class="site-header">
+        <div class="container">
+            <h1 class="school-logo-text float-left">
+                <a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
+            </h1>
+            <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+            <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+            <div class="site-header__menu group">
+                <nav class="main-navigation">
+                    <ul>
+                        <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 16) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+                        <li <?php if (get_post_type() == 'program') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('program') ?>">Programs</a></li>
+                        <li <?php if (get_post_type() == 'event' or is_page('past-events')) echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
+                        <li><a href="#">Campuses</a></li>
+                        <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
+                    </ul>
+                </nav>
+                <div class="site-header__util">
+                    <?php
+                    if (is_user_logged_in()) { ?>
+                        <a href="<?php echo esc_url(site_url('/my-notes')); ?>" class="btn btn--small btn--orange float-left">My notes</a>
+                        <a href="<?php echo wp_logout_url(); ?>" class="btn btn--small btn--dark-orange float-left btn--with-photo">
+                            <span class="site-header__avatar"><?php echo get_avatar(get_current_user(), 60); ?></span>
+                            <span class="btn__text">Log Out</span>
+                        </a>
+                    <?php
+                    } else { ?>
+                        <a href="<?php echo wp_login_url(); ?>" class="btn btn--small btn--orange float-left">Login</a>
+                        <a href="<?php echo wp_registration_url(); ?>" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
+                    <?php
+                    }
+                    ?>
+                    <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+                </div>
+            </div>
+        </div>
+    </header>
---
+++ js/modules/Search.js
@@ -0,0 +1,196 @@
+import $ from "jquery";
+
+class Search {
+  constructor() {
+    this.addSearchHTML();
+    this.openButton = $(".js-search-trigger");
+    this.closeButton = $(".search-overlay__close");
+    this.searchOverlay = $(".search-overlay");
+    this.searchField = $("#search-term");
+    this.resultsDiv = $("#search-overlay__resutls");
+    this.isOverlayOpen = false;
+    this.typingTimer = null;
+    this.isLoading = false;
+    this.prevValue = "";
+    this.events();
+  }
+
+  // evetns
+  events() {
+    this.openButton.on("click", this.openOverlay.bind(this));
+    this.closeButton.on("click", this.closeOverlay.bind(this));
+    $(document).on("keydown", this.keyPressDispatcher.bind(this));
+    this.searchField.on("keyup", this.typingLogic.bind(this));
+  }
+
+  // methods
+  typingLogic() {
+    if (this.searchField.val() !== this.prevValue) {
+      clearTimeout(this.typingTimer);
+
+      if (this.searchField.val()) {
+        if (!this.isLoading) {
+          this.resultsDiv.html('<div class="spinner-loader"></div>');
+          this.isLoading = true;
+        }
+        this.typingTimer = setTimeout(this.getResults.bind(this), 750);
+      } else {
+        this.resultsDiv.html("");
+        this.isLoading = false;
+      }
+    }
+    this.prevValue = this.searchField.val();
+  }
+
+  getResults() {
+    $.getJSON(
+      `${
+        univeristyData.root_url
+      }/wp-json/university/v1/search?term=${this.searchField.val()}`,
+      (results) => {
+        console.log(results);
+        this.resultsDiv.html(`
+        <div class="row">
+          <div class="one-third">
+            <h2 class="search-overlay__section-title">General Information</h2>
+            ${
+              results.generalInfo.length
+                ? '<ul class="link-list min-list">'
+                : "<p>No General Information</p>"
+            }
+            ${results.generalInfo
+              .map(
+                (item) =>
+                  `<li><a href="${item.permalink}">${item.title}</a> ${
+                    item.postType == "post" ? `by ${item.authorName}` : ""
+                  }</li>`
+              )
+              .join("")}
+            ${results.generalInfo.length ? "</ul>" : ""}
+          </div>
+          <div class="one-third">
+            <h2 class="search-overlay__section-title">Programs</h2>
+            ${
+              results.programs.length
+                ? '<ul class="link-list min-list">'
+                : `<p>No programs match that search. <a href='${univeristyData.root_url}/programs'>View all programs</a></p>`
+            }
+            ${results.programs
+              .map(
+                (item) =>
+                  `<li><a href="${item.permalink}">${item.title}</a></li>`
+              )
+              .join("")}
+            ${results.programs.length ? "</ul>" : ""}
+            <h2 class="search-overlay__section-title">Professors</h2>
+            ${
+              results.professors.length
+                ? '<ul class="link-list min-list">'
+                : `<p>No professors match that search.</p>`
+            }
+            ${results.professors
+              .map(
+                (item) =>
+                  `<li class="professor-card__list-item">
+                    <a class="professor-card" href="${item.permalink}">
+                        <img class="professor-card__image" src="${item.image}" />
+                        <span class="professor-card__name">${item.title}</span>
+                    </a>
+                  </li>`
+              )
+              .join("")}
+            ${results.professors.length ? "</ul>" : ""}
+          </div>
+          <div class="one-third">
+          <!-- CAMPUS TYPE -->
+            <h2 class="search-overlay__section-title">Campuses</h2>
+            ${
+              results.campuses.length
+                ? '<ul class="link-list min-list">'
+                : `<p>No campuses match that search. <a href='${univeristyData.root_url}/campuses'>View all campuses</a></p>`
+            }
+            ${results.campuses
+              .map(
+                (item) =>
+                  `<li><a href="${item.permalink}">${item.title}</a></li>`
+              )
+              .join("")}
+            ${results.campuses.length ? "</ul>" : ""}
+            <h2 class="search-overlay__section-title">Events</h2>
+            ${
+              results.events.length
+                ? ""
+                : `<p>No evetns match that search. <a href='${univeristyData.root_url}/events'>View all events</a></p>`
+            }
+            ${results.events
+              .map(
+                (item) => `
+                <div class="event-summary">
+                  <a class="event-summary__date t-center" href="${item.permalink}">
+                      <span class="event-summary__month">
+                        ${item.month}
+                      </span>
+                      <span class="event-summary__day">${item.day}</span>
+                  </a>
+                  <div class="event-summary__content">
+                      <h5 class="event-summary__title headline headline--tiny"><a href="${item.permalink}">${item.title}</a></h5>
+                      <p>${item.description}<a href="${item.permalink}" class="nu gray">Learn more</a></p>
+                  </div>
+                </div>
+            `
+              )
+              .join("")}
+          </div>
+        </div>
+      `);
+        this.isLoading = false;
+      }
+    );
+  }
+
+  openOverlay() {
+    this.searchOverlay.addClass("search-overlay--active");
+    $("body").addClass("body-no-scroll");
+    setTimeout(() => {
+      this.searchField.focus();
+    }, 301);
+    this.isOverlayOpen = true;
+  }
+
+  closeOverlay() {
+    this.searchOverlay.removeClass("search-overlay--active");
+    $("body").removeClass("body-no-scroll");
+    this.isOverlayOpen = false;
+  }
+
+  keyPressDispatcher(e) {
+    if (
+      e.keyCode === 83 &&
+      !this.isOverlayOpen &&
+      !$("input, textarea").is(":focus")
+    ) {
+      this.openOverlay();
+    } else if (e.keyCode === 27 && this.isOverlayOpen) {
+      this.closeOverlay();
+    }
+  }
+
+  addSearchHTML() {
+    $("body").append(`
+          <div class="search-overlay">
+          <div class="search-overlay__top">
+          <div class="container">
+              <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+              <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+              <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term" />
+          </div>
+          </div>
+          <div class="container">
+          <div id="search-overlay__resutls"></div>
+          </div>
+      </div>
+      `);
+  }
+}
+
+export default Search;
